275d05b8d0beb4ace7587f95567887b5dbaacafe
Lypeix <237925541+Lypeix@users.noreply.github.com>
2026-07-20 12:00:45 +0200

    Put pydantic schema into schemas n import to main

---
+++ schemas.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, Field
+
+class CelestialObjectCreate(BaseModel):
+    name: str = Field(min_length=1, max_length=100)
+    object_type: str = Field(min_length=1, max_length=100)
+    distance_light_years: float = Field(ge=0)
+    potentially_habitable: bool
+    description: str = Field(min_length=1, max_length=500)
